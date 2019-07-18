7a4d6b0678ab1adba43bcd3d5ff42e2b65c113ca
Arjun Nemani <arjun.fall2018@gmail.com>
2019-07-18 22:40:44 +0200

    Update DetailViewController.swift

--- ContactList-App/DetailViewController.swift
+++ ContactList-App/DetailViewController.swift
@@ -22,7 +22,7 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
-        // Do any additional setup after loading the view.
+        .
 
     }
     
